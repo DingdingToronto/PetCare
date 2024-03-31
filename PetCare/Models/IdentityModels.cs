c557199ad7037f96a32cac1f4aaa0e580c8149a5
DingdingToronto <jiabaoding6@gmail.com>
2024-03-31 11:54:45 -0400

    project init

---
+++ PetCare/Models/IdentityModels.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PetCare.Models
+{
+    // 可以通过将更多属性添加到 ApplicationUser 类来为用户添加配置文件数据，请访问 https://go.microsoft.com/fwlink/?LinkID=317594 了解详细信息。
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // 请注意，authenticationType 必须与 CookieAuthenticationOptions.AuthenticationType 中定义的相应项匹配
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // 在此处添加自定义用户声明
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        //Add an animal entity to our system
+        public DbSet<Vet> Vets { get; set; }
+
+        
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ PetCare/Models/Vet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetCare.Models
+{
+    public class Vet
+    {
+        [Key]
+        public int VetID { get; set; }
+        public string VetName { get; set; }
+
+        public string Specialization { get; set; }
+
+        public string Phone { get; set; }
+
+
+        public string Email { get; set; }
+
+
+        public string Address { get; set; }
+
+        public int Rating { get; set; }
+
+    
+    }
+}
